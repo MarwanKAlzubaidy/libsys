73e9afa3b6a90828145a925e8b95e8d185627515
SleepyCyclops <mar20122102@gmail.com>
2021-11-18 06:53:19 +0300

    Initial commit project start up

---
+++ src/main/java/edu/kfupm/libsys/LibsysApplication.java
@@ -0,0 +1,13 @@
+package edu.kfupm.libsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibsysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibsysApplication.class, args);
+    }
+
+}
