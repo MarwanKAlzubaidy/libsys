2539b673628acf13dfde305e39bca9c8116b3415
Abdullah Irqsous <91019200+IRQSO@users.noreply.github.com>
2021-12-20 00:14:01 +0300

    Added PublisherService

---
+++ src/main/java/ics324/project/libsys/entities/services/PublisherService.java
@@ -0,0 +1,22 @@
+package ics324.project.libsys.entities.services;
+
+import ics324.project.libsys.entities.Library;
+import ics324.project.libsys.entities.Publisher;
+import ics324.project.libsys.repo.LibraryRepository;
+import ics324.project.libsys.repo.PublisherRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PublisherService {
+    private PublisherRepository publisherRepository;
+
+    public PublisherService(PublisherRepository publisherRepository){this.publisherRepository = publisherRepository;}
+
+    public List<Publisher> getAllPublishers() { return publisherRepository.findAll(); }
+
+    public void savePublisher(Publisher publisher) { publisherRepository.save(publisher); }
+
+    public void deletePublisher(Publisher publisher) { publisherRepository.delete(publisher); }
+}
